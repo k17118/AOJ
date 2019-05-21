24662fb68b921070853b670c09dde9b9218f4714
Kipple <k17118kk@aitech.ac.jp>
2019-05-21 10:11:36 +0900

    ITP1_7_Dを4.cpp書き込んだ

--- 4.cpp
+++ 4.cpp
@@ -11,27 +11,29 @@ int main(){
 
   cin >> n >> m >> l;
 
-  for(int i = 0; i < n; i++){
-    for(int j = 0; j < m; j++){
-      cin >> a[i][j];
-      cout << a[i][j] << endl;
+  for(int in = 0; in < n; in++){
+    for(int jm = 0; jm < m; jm++){
+      cin >> a[in][jm];
     }
   }
-  for(int i = 0; i < m; i++){
-    for(int j = 0; j < l; j++){
-      cin >> b[j][l];
-      cout << b[j][l] << endl;
-
+  for(int jm = 0; jm < m; jm++){
+    for(int kl = 0; kl < l; kl++){
+      cin >> b[jm][kl];
     }
   }
 
   for(int i = 0; i < n; i++){
     for(int j = 0; j < m; j++){
       for(int k = 0; k < l; k++){
-        c[i][j] = a[i][j] * b[j][k];
+        c[i][k] += a[i][j] * b[j][k];
       }
     }
   }
 
-
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < l; j++){
+      cout << c[i][j] << " ";
+    }
+    cout << endl;
+  }
 }
